ba5e802af05c662217c8644d5f9bbbd940162129
lucasmetron <lucasmetron@gmail.com>
2021-05-17 15:56:13 -0300

    empress páginas estaticas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require('path');
+
 const app = express();
 
 const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'client'))) //função path.join() junta o endereço |___dirname substitui o ./ | 'client' é o nome da pasta onde estão os arquivos estáticos.
+// app.use(express.static("C:/Users/dell/Desktop/ProgWeb/mod22NodeExpress/client")) //poderia ser assim
 
 app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`)
