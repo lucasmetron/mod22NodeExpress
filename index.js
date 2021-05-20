55f4e935a4b37e952a7c6d3dc904cdd0ad2d55f7
lucasmetron <lucasmetron@gmail.com>
2021-05-20 16:50:34 -0300

    rotas

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-const express = require("express");
-const path = require('path');
+var express = require("express");
+var app = express();
 
-const app = express(); //Desta forma inicializa o objeto express.
+let alunos = [
+    { id: 0, nome: "José" },
+    { id: 1, nome: "Maria" },
+    { id: 2, nome: "João" },
+    { id: 3, nome: "Marcos" }
+]
 
-var bodyParser = require("body-parser")
+app.use(express.urlencoded());
 
-let consoleBody = (req, res, next) => {
-    console.log(req.body);
-    next();
-}
+app.get("/", (req, res) => {
 
-let mid = (req, res) => {
-    res.type("html")
-    res.send(`<h1>Hello world!`)
-};
-
-
-app.use("/meusite", express.static(path.join(__dirname, 'client'))) //função path.join() junta o endereço |___dirname substitui o ./ | 'client' é o nome da pasta onde estão os arquivos estáticos.
-
-app.use("/", express.json())
-
-app.use("/", consoleBody)
-
-app.get("/", mid)
-
-app.put("/", mid)
-
-app.post("/", mid)
-
-app.delete("/", mid)
-
-
-const PORT = 5000;
-app.listen(PORT, () => { //Fica escutando a porta 3000
-    console.log(`Server running on Port: ${PORT}`)
+    res.send("Hello wolrd! TESTE :)")
 })
+
+
+app.get("/alunos", (req, res) => {
+
+    res.json(JSON.stringify(alunos))
+})
+
+app.get("/aluno", (req, res) => {
+    console.log(req.body);
+    // console.log(req.query) Desta forma deve ser passado o id aluno na url como ex: /aluno?id=0
+    let aluno = alunos[req.body.id];
+    // let aluno = alunos[req.query.id]; caso use query. 
+    res.json(aluno);
+})
+
+app.get("/aluno/:id", (req, res) => { //Desta maneira, é possivel passar o id do aluno como ex: /aluno/2
+    console.log(req.params);
+    let aluno = alunos[req.params.id];
+    res.json(aluno);
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando...")
+})
+
